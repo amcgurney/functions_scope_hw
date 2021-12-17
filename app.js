65cded67f04a6f379deeb79a03a066ecd10ddb97
Alan McGurney <mcgurneyalan@gmail.com>
2021-12-17 17:41:48 -0500

    commit 1 - verbal ?'s

---
+++ app.js
@@ -0,0 +1,3 @@
+// 1. Parameters are variables that are part of a function. An argument is a value that goes to the function when called.
+// 2. Return will, apparently, return a value from a function while console.log prints info sent to the console.
+// 3. The function returns a value to the script that called it once it's completed.
